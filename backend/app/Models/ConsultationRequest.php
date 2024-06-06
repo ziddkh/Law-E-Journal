a1c8036ae08b5d66520d78bd0f8ec13b1f6145ab
“Virgighal” <virgig@thebigrichgroup.com>
2024-06-07 04:19:38 +0700

    backend : consultation request

---
+++ backend/app/Models/ConsultationRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+class ConsultationRequest extends Model
+{
+    use SoftDeletes, LogsActivity;
+
+    protected $fillable = [
+        'name',
+        'phone_number',
+        'email',
+        'message'
+    ];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()->logAll()->logOnlyDirty();
+    }
+}
