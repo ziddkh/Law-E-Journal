66e86481b55439f6469201e26fbdbabf8ca713c7
Ziyadatul Khair <ziyadatulk@thebigrichgroup.com>
2024-07-01 21:50:53 +0700

    update

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -12,7 +12,6 @@ import { Observable, filter, tap } from 'rxjs';
 })
 export class AppComponent implements OnInit {
   title = 'law-e-journal-frontend-new';
-  private maxLoadTime = 10000
 
   constructor(
     private settingApi: SettingApi,
@@ -25,19 +24,6 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.getSettings();
-
-    (this.router.events as Observable<RouterEvent>).pipe(
-      filter((event: RouterEvent) => event instanceof NavigationStart || event instanceof NavigationEnd),
-      tap((event: RouterEvent) => {
-        if (event instanceof NavigationStart) {
-          this.renderer.setStyle(document.body, 'overflow', 'hidden');
-          this.loaderService.show(this.maxLoadTime);
-        }
-        if (event instanceof NavigationEnd) {
-          this.checkIfContentLoaded();
-        }
-      })
-    ).subscribe();
   }
 
   getSettings() {
@@ -50,74 +36,5 @@ export class AppComponent implements OnInit {
       })
   }
 
-  checkIfContentLoaded() {
-    const elements = [
-      ...Array.from(document.images),
-      ...Array.from(document.querySelectorAll('iframe')),
-      ...Array.from(document.scripts),
-      ...Array.from(document.styleSheets)
-    ];
 
-    let loadedElements = 0;
-    const totalElements = elements.length;
-
-    if (totalElements === 0) {
-      this.loaderService.hide();
-      this.renderer.removeStyle(document.body, 'overflow');
-      return;
-    }
-
-    elements.forEach(element => {
-      if (element instanceof HTMLImageElement) {
-        if (element.complete) {
-          loadedElements++;
-          if (loadedElements === totalElements) {
-            this.loaderService.hide();
-            this.renderer.removeStyle(document.body, 'overflow');
-          }
-        } else {
-          this.ngZone.runOutsideAngular(() => {
-            this.renderer.listen(element, 'load', () => {
-              this.ngZone.run(() => {
-                loadedElements++;
-                if (loadedElements === totalElements) {
-                  this.loaderService.hide();
-                  this.renderer.removeStyle(document.body, 'overflow');
-                }
-              });
-            });
-          });
-        }
-      } else if (element instanceof HTMLIFrameElement) {
-        if (element.contentWindow && element.contentWindow.document.readyState === 'complete') {
-          loadedElements++;
-          if (loadedElements === totalElements) {
-            this.loaderService.hide();
-            this.renderer.removeStyle(document.body, 'overflow');
-          }
-        } else {
-          this.ngZone.runOutsideAngular(() => {
-            this.renderer.listen(element, 'load', () => {
-              this.ngZone.run(() => {
-                loadedElements++;
-                if (loadedElements === totalElements) {
-                  this.loaderService.hide();
-                  this.renderer.removeStyle(document.body, 'overflow');
-                }
-              });
-            });
-          });
-        }
-      }
-      else if (element instanceof HTMLScriptElement || element instanceof CSSStyleSheet) {
-        // Assuming scripts and stylesheets are loaded if they are already in the document
-        loadedElements++;
-        if (loadedElements === totalElements) {
-          this.loaderService.hide();
-          this.renderer.removeStyle(document.body, 'overflow');
-        }
-      }
-    });
-
-  }
 }
